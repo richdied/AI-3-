6d25ab0939127de891d0e6a084051aed9e623450
SungAe <star__0910@naver.com>
2022-02-14 12:16:17 +0900

    게시물 수정 및 삭제

--- projects/mysite/pybo/models.py
+++ projects/mysite/pybo/models.py
@@ -5,6 +5,7 @@ class Question(models.Model):
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
@@ -15,4 +16,6 @@ class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+
 
